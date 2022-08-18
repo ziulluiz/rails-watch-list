63ad517b2353d8fdad51655e3020032cfcb75414
Luis Enrique Guzman Pineda <luis.guzp13@gmail.com>
2022-08-18 18:01:23 -0500

    movie API

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,6 @@
 require 'json'
 class MoviesController < ApplicationController
   def index
-    url = "https://tmdb.lewagon.com/movie/top_rated".freeze
-    @movies = JSON.parse(URI.open(url).read)
+   @movies = Movie.all
   end
 end
